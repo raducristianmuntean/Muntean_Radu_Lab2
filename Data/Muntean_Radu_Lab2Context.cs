24add8aef30f85a63828ea19c0b9e22831b9edac
Radu Muntean <mr.radu.muntean@gmail.com>
2025-11-17 14:49:11 +0200

    Add initial changes

---
+++ Data/Muntean_Radu_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Muntean_Radu_Lab2.Models;
+
+namespace Muntean_Radu_Lab2.Data
+{
+    public class Muntean_Radu_Lab2Context : DbContext
+    {
+        public Muntean_Radu_Lab2Context (DbContextOptions<Muntean_Radu_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Muntean_Radu_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
