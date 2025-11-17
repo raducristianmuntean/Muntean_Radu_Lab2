0b9e72a5281bef8296457ec77e324788eded2691
Radu Muntean <mr.radu.muntean@gmail.com>
2025-11-17 14:28:13 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Muntean_Radu_Lab2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
