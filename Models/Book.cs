2adb8304fea6f36f2fedd718bea68eda031acf30
Radu Muntean <mr.radu.muntean@gmail.com>
2025-11-17 15:21:16 +0200

    Additional changes

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,10 +1,18 @@
-﻿namespace Muntean_Radu_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Muntean_Radu_Lab2.Models
 {
     public class Book
     {
         public int ID { get; set; }
+        [Display(Name = "Book Title")]
         public string Title { get; set; }
         public string Author { get; set; }
+        [Column(TypeName = "decimal(6, 2)")]
         public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime PublishingDate { get; set; }
     }
 }
