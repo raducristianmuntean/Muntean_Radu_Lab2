9f94c617087c6241a7cf0f1805a1a87b1b908956
Radu Muntean <mr.radu.muntean@gmail.com>
2025-12-10 17:03:39 +0200

    members and borrowings

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,24 +1,28 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Security.Policy;
 
 namespace Muntean_Radu_Lab2.Models
 {
     public class Book
     {
         public int ID { get; set; }
+        
         [Display(Name = "Book Title")]
         public string Title { get; set; }
-        public decimal Price { get; set; }
 
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+        
         [DataType(DataType.Date)]
         public DateTime PublishingDate { get; set; }
-        public int? PublisherID { get; set; }
-        public Publisher? Publisher { get; set; }
+        
         public int? AuthorID { get; set; }
         public Author? Author { get; set; }
-
+        
+        public int? PublisherID { get; set; }
+        public Publisher? Publisher { get; set; }
+        
+        public ICollection<Borrowing>? Borrowings { get; set; }
         public ICollection<BookCategory>? BookCategories { get; set; }
-
     }
 }
---
+++ Pages/Borrowings/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Muntean_Radu_Lab2.Data;
+using Muntean_Radu_Lab2.Models;
+
+namespace Muntean_Radu_Lab2.Pages.Borrowings
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Muntean_Radu_Lab2.Data.Muntean_Radu_Lab2Context _context;
+
+        public IndexModel(Muntean_Radu_Lab2.Data.Muntean_Radu_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Borrowing> Borrowing { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Borrowing = await _context.Borrowing
+                .Include(b => b.Book)
+                .Include(b => b.Member).ToListAsync();
+        }
+    }
+}
